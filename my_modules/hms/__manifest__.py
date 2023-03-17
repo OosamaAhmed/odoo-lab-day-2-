c3ff9e2deac7b8dcedfffee1e3cd21ac7e6a2e56
OsamaAhmed187 <osamaahmedalkatkoti@gmail.com>
2023-03-17 18:09:39 +0200

    odoo day two lab

---
+++ my_modules/hms/__manifest__.py
@@ -0,0 +1,13 @@
+{
+
+    'name':"hms_odoo",
+    'version':'1.0',
+    'data' : [
+    'views/createpatients.xml',
+      'views/departments.xml',
+        'views/doctor.xml',
+    ]
+}
+
+
+# python3 odoo/odoo-bin -c odoo.conf -d odoo_db
